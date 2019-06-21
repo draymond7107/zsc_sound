a3e58208f04b72d0b4422e9a158f943586d89e7b
张苏超 <draymond7107@sina.com>
2019-06-21 11:20:42 +0800

    maven项目搭建

---
+++ src/main/java/springMVC/TestMain.java
@@ -0,0 +1,14 @@
+package springMVC;
+
+
+/**
+ * @author ZhangSuchao
+ * @create 2019/6/21
+ * @since 1.0.0
+ */
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+}
